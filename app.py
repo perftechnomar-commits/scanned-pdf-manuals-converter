bd2f5b0469470304e6ff2dc08401fbd1708f8f7d
perftechnomar-commits <perftechnomar@gmail.com>
2026-08-20 13:08:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ except ImportError:
 
 APP_DIR = Path(__file__).resolve().parent
 DEFAULT_TEMPLATE_PATH = APP_DIR / "Spare parts template last version.xlsx"
-APP_VERSION = "4.14.4"
+APP_VERSION = "4.14.5"
 
 DEFAULT_VESSEL_PATH = APP_DIR / "vessels.csv"
 
