93bffeea05f68429dd05c992114b04ec670cf295
perftechnomar-commits <perftechnomar@gmail.com>
2026-08-28 12:23:09 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -19,7 +19,7 @@ from pypdf import PdfReader, PdfWriter
 from pypdf.generic import RectangleObject
 
 
-TOOLS_VERSION = "4.18.9"
+TOOLS_VERSION = "4.18.10"
 
 MACHINERY_SHEET = "1.Machineries|Sub|Units"
 SPARE_PARTS_SHEET = "2.Spare Parts"
